f600762c473eeb6cc8bc5cb26fa8d267a761d349
Renan Santos de Siqueira <renan.siqueira@riachuelo.com.br>
2021-05-05 22:28:42 -0300

    Finalizando a chamada de Debito

--- saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/resources/SaldoExtratoResource.java
+++ saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/resources/SaldoExtratoResource.java
@@ -1,17 +1,19 @@
 package br.com.impacta.fullstack.saldoextrato.resources;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
+import br.com.impacta.fullstack.saldoextrato.domain.Extrato;
 import br.com.impacta.fullstack.saldoextrato.dto.DebitDTO;
 import br.com.impacta.fullstack.saldoextrato.services.SaldoExtratoService;
 
@@ -22,14 +24,20 @@ public class SaldoExtratoResource {
 	@Autowired
 	private SaldoExtratoService service;
 
-	@HystrixCommand(fallbackMethod = "getDebitoAlternative")
+//	@HystrixCommand(fallbackMethod = "getDebitoAlternative")
 	@PostMapping
 	public ResponseEntity<Void> insert(@Valid @RequestBody DebitDTO objDto) {
 		service.insert(objDto);
 		return ResponseEntity.noContent().build();
 	}
 	
-	@HystrixCommand(fallbackMethod = "getDebitoAlternative")
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<List<Extrato>> findDebitById(@PathVariable Long id) {
+		List<Extrato> obj = service.findExtratoById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+//	@HystrixCommand(fallbackMethod = "getDebitoAlternative")
 	@GetMapping(value = "/debito")
 	public ResponseEntity<String> getHelloDebit(){
 		ResponseEntity<String> hello = service.getHelloDebit();
--- saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/services/SaldoExtratoService.java
+++ saldoextrato/src/main/java/br/com/impacta/fullstack/saldoextrato/services/SaldoExtratoService.java
@@ -1,9 +1,12 @@
 package br.com.impacta.fullstack.saldoextrato.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.impacta.fullstack.saldoextrato.domain.Extrato;
 import br.com.impacta.fullstack.saldoextrato.dto.DebitDTO;
 import br.com.impacta.fullstack.saldoextrato.feignclients.CreditoFeignClient;
 import br.com.impacta.fullstack.saldoextrato.feignclients.DebitoFeignClient;
@@ -27,6 +30,10 @@ public class SaldoExtratoService {
 	
 	public ResponseEntity<Void> insert(DebitDTO obj) {
 		return (ResponseEntity<Void>) debitoFeignClient.insert(obj);
+	}
+
+	public List<Extrato> findExtratoById(Long id) {
+		return debitoFeignClient.findExtratoById(id);
 	}
 	
 	
